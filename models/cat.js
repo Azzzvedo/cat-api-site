440ab7ffc3fe9b0f6637272c62f1d28920cede1c
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-16 16:58:26 +0100

    exploring api

--- models/cat.js
+++ models/cat.js
@@ -3,11 +3,22 @@ const axios = require('axios');
 module.exports = class Cat {
   static async fetchRandomCat() {
     try {
-      const response = await axios({
-        url: '/search',
-        method: 'get', // default
-        baseURL: 'https://api.thecatapi.com/v1/images',
+      const response = await axios.get('/v1/images/search', {
+        baseURL: 'https://api.thecatapi.com',
         // headers: { 'x\-api\-key': process.env.CAT_API_KEY }
+      })
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  static async fetchCatBreeds() {
+    try {
+      const response = await axios({
+        url: '/v1/breeds',
+        method: 'get', // default
+        baseURL: 'https://api.thecatapi.com',
+        // headers: { "x-api-key": process.env.CAT_API_KEY }
       })
       return response.data;
     } catch (error) {
--- routes/cats.js
+++ routes/cats.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 const catsController = require("../controllers/cats")
 
+router.get('/breeds', catsController.getBreeds)
 router.get('/', catsController.getIndex)
 
 module.exports = router
